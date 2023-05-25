3d83d286c53b216ae3e9a32fa6ffbac0758f5af2
Bruno Pasquetti <brunopasquetti16@gmail.com>
2023-05-24 21:14:46 -0300

    Aula.git

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time 
+import webbrowser
+pyautogui.press("win")
+pyautogui.write("Firefox")
+
+pyautogui.press("backspace")
+pyautogui.press ("enter")
+time.sleep(1)
+pyautogui.write("yotube music")
+pyautogui.press("backspace")
+pyautogui.press ("enter")
+time.sleep (4)
+pyautogui.click(x=380, y=630)
+time.sleep(4)
+pyautogui.click(x=350, y=650)
+
